769e670cd3c1ed81c80297f00d99d46a232446e6
Marcela Romero <marcelaromero@live.com>
2016-02-24 16:16:27 -0300

    modificadores de acesso

---
+++ src/Aula31alt/TesteCarroAlt.java
@@ -0,0 +1,11 @@
+package Aula31alt;
+
+import Aula31.Carro;
+
+public class TesteCarroAlt {
+
+    public static void main(String[] args){
+        Carro carro = new Carro();
+        carro.marca = "Fiat"; // é possivel porque declarei o atributo marca na aula 31 como public
+    }
+}
